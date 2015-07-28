ffc8c111ec62a04588a74d4b792d002361a5906f
Reinhard Engels <reinhard.engels@gmail.com>
2015-07-28 16:49:19 -0400

    Move sample dataSources into separate file

---
+++ app/data/dataSources.js
@@ -0,0 +1,5 @@
+var dataSources = [ 
+      {'name': 'Dev',             'host': 'http://54.173.15.205',  'dataUrl':'data/54.173.15.205.json'                   },
+      {'name': 'Stage',           'host': 'http://54.85.122.143',  'dataUrl':'data/54.85.122.143.json'                   },
+      {'name': 'Prod',            'host': 'http://54.84.99.24'  ,  'dataUrl':'data/54.84.99.24.json'                     }
+      ];
